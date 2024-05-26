89b1662f544970cdea7b3162b16eb5ca103f8191
darylk <daryl.kkuchay@icloud.com>
2024-05-26 13:44:38 -0400

    still Javascript

---
+++ calculateArea/groceryTotaler.js
@@ -0,0 +1,9 @@
+let Grocery1;
+let Grocery2;
+let Grocery3;
+function groceryTotaler() {
+        let total = Grocery1 + Grocery2 + Grocery3;
+        return total;
+       }
+
+document.getElementById('total').innerText = `The total amount is: ${total}`;
---
+++ productSurvey/product_survey.js
@@ -0,0 +1,12 @@
+function submitFeedback() {
+    const username = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+    const email = document.getElementById('email').value;
+    const job = document.getElementById('job').value;
+    const designation = document.getElementById('designation').value;
+    const productType = document.getElementById('productType').value;
+    const feedback = document.getElementById('feedbackText').value;  
+}
+const submitButton=document.getElementById('submitBtn');
+submitButton.onclick = submitFeedback;
+alert('Thank you for your valuable feedback')
